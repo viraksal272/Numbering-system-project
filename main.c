3a3f0fe61a77659cb0b6f368f442e98bcca0dfdd
viraksal <viraksal272@gmail.com>
2025-08-16 19:49:10 +0700

    new code from bro tong heng

--- main.c
+++ main.c
@@ -1,151 +1,113 @@
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
+
+void decimalToBinary(char deciNum[50]) {
+    char integerPart[20];
+    char fractionalPart[20];
+    char fractionalFloatStr[25];
+    char *dotPosition = strchr(deciNum, '.');
+
+    if (dotPosition != NULL) {
+        size_t intLen = dotPosition - deciNum;
+        strncpy(integerPart, deciNum, intLen);
+        integerPart[intLen] = '\0';
+        int place = 1;
+        int intBinaNum = 0;
+        int count = 0;
+        int len = 0;
+        char fracBinaStr[50] = "";
+
+        strcpy(fractionalPart, dotPosition + 1);
+        int intValue = atoi(integerPart);
+
+        snprintf(fractionalFloatStr, sizeof(fractionalFloatStr), "0.%s", fractionalPart);
+        float fractionalValue = atof(fractionalFloatStr);
+
+        while (intValue > 0) {
+          int remainder = intValue % 2;
+          intBinaNum += remainder * place;
+          place *= 10;
+          intValue /= 2;
+        }
+
+        while (count <= 15) {
+          float eachResult = fractionalValue * 2;
+          int digit = (int)eachResult;
+          fractionalValue = eachResult - digit;
+
+          int len = strlen(fracBinaStr);
+          fracBinaStr[len] = digit + '0';
+          fracBinaStr[len + 1] = '\0';
+
+          if (eachResult == 1.00) break;
+
+          count++;
+        }
+
+        printf("Decimal: %s\n", deciNum);
+        printf("Binary: %d.%s\n", intBinaNum, fracBinaStr);
+    } else {
+        int binaResult = 0;
+        int place = 1;
+        strcpy(integerPart, deciNum);
+        fractionalPart[0] = '\0';
+
+        int intValue = atoi(integerPart);
+
+        while (intValue > 0) {
+          int remainder = intValue % 2;
+          binaResult += remainder * place;
+          place *= 10;
+          intValue /= 2;
+        }
+
+        printf("Decimal: %s\n", deciNum);
+        printf("Binary: %d\n", binaResult);
+    }
+}
+
 int main() {
-    char text[]="\033[1mNUMBER CONVERTER\033[0m";
-    int width = 120;
-    int space = (width - strlen(text)) / 2;
-    int choice;
-    for (int i = 0; i < space; i++) {
-        printf(" ");
-    }
-    printf("%s\n\n", text);
-    printf("Please select the type of conversion you want to do\n\n");
-    printf("1.Decimal to Binary\n\n");
-    printf("2.Decimal to Octal\n\n");
-    printf("3.Decimal to Hexadecimal\n\n");
-    printf("4.Binary to Decimal\n\n");
-    printf("Enter your choice(number only) : ");
-    scanf("%d",&choice);
 
-    //to seperate all the tasks to different part
+    while (true) {
+      printf("=========================================\n");
+      printf("| A -> Decimal to Binary\t\t|\n");
+      printf("| B -> Decimal to Octal\t\t\t|\n");
+      printf("| C -> Decimal to Hexadecimal\t\t|\n");
+      printf("| Press ESC key to exit!\t\t|\n");
+      printf("=========================================\n");
+      char ch; // get choice from the user
+      printf("Enter your choice: ");
 
-    switch (choice) {
-        case 1:
-            printf("Decimal to Binary\n\n");
-            DecToBin();
-            break;
-        case 2:
-            printf("Decimal to Octal\n\n");
-            DecToOctal();
-            break;
-        case 3:
-            printf("Decimal to Hexadecimal\n\n");
-            DecToHex();
-            break;
-        case 4:
-            printf("Binary to Decimal\n\n");
-            BinToDec();
-            break;
+      ch = getch();
+
+      printf("\n");
+      if (ch == 27) {
+        printf("Exiting program...!\n");
+        break;
+      }
+
+      printf("\n");
+      switch (ch){
+        case 'A':
+        case 'a':
+                char deciNum[50];
+
+                printf("Enter decimal number: ");
+                fgets(deciNum, sizeof(deciNum), stdin);
+
+                size_t len = strlen(deciNum);
+                if (len > 0 && deciNum[len - 1] == '\n') {
+                    deciNum[len - 1] = '\0';
+                }
+                decimalToBinary(deciNum);
+                break;
         default:
-            printf("Invalid Choice\n\n");
-            break;
+          printf("Invalid input...!\n");
+          break;
+      }
     }
-    system("pause");
     return 0;
 }
-
-//to write the code that make the program works
-
-void DecToBin() {
-    int num;
-    int binary[32];
-    int i = 0;
-    char text[]="\033[1mHere you can convert Decimal to Binary\033[0m";
-    int width = 120;
-    int space = (width - strlen(text)) / 2;
-    int choice;
-    for (int i = 0; i < space; i++) {
-        printf(" ");
-    }
-    printf("%s\n\n",text);
-    printf("Please enter your Decimal number : ");
-    scanf("%llu",&num);
-    if (num == 0) {
-        printf("Binary = 0\n");
-        return;
-    }
-    printf("\n");
-    while (num > 0) {
-        binary[i] = num % 2;
-        num = num / 2;
-        i++;
-    }
-    printf("Binary = ");
-    for (int j = 0; j < i; j++) {
-        printf("%d", binary[j]);
-    }
-    printf("\n\n");
-}
-void DecToOctal() {
-    int num;
-    int octal[32];
-    int i = 0;
-    char text[]="\033[1mHere you can convert Decimal to OctalDecimal\033[0m";
-    int width = 120;
-    int space = (width - strlen(text)) / 2;
-    int choice;
-    for (int i = 0; i < space; i++) {
-        printf(" ");
-    }
-    printf("%s\n\n",text);
-    printf("Please enter your Decimal Number : ");
-    scanf("%llu",&num);
-
-
-}
-void DecToHex() {
-    int num;
-    int decimal[32];
-    int i = 0;
-    char text[]="\033[1mHere you can convert Decimal to HexaDecimal\033[0m";
-    int width = 120;
-    int space = (width - strlen(text)) / 2;
-    int choice;
-    for (int i = 0; i < space; i++) {
-        printf(" ");
-    }
-    printf("%s\n\n",text);
-    printf("Please enter your Decimal number : ");
-    scanf("%llu",&num);
-    //Please write the algorithm here
-}
-
-void BinToDec() {
-    unsigned long long binary;
-    int Decimal = 0;
-    int base = 1;
-    int remainder;
-    char text[] = "\033[1mHere you can convert Binary to Decimal\033[0m";
-    int width = 120;
-    int space = (width - strlen(text)) / 2;
-    int choice;
-    for (int i = 0; i < space; i++) {
-        printf(" ");
-    }
-    printf("%s\n\n", text);
-    printf("Please enter your Binary number : ");
-    scanf("%llu", &binary);
-    unsigned long long temp = binary;   //start to check if the binary is valid
-    int valid = 1;
-    while (temp > 0) {
-        int digit = temp % 10;
-        if (digit !=0 && digit != 1) {
-            valid = 0;
-            break;
-        }
-        temp /= 10;
-    }
-    if (!valid){
-        printf("\n\nInvalid Binary Number, Please try again\n\n");
-        return;
-    }
-        while (binary > 0) {           //conversion
-            remainder = binary % 10;
-            Decimal += remainder * base;
-            base *= 2;
-            binary /= 10;
-        }
-        printf("Decimal = %d\n ", Decimal);
-    }
